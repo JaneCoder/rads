d6f124b027826e59f9f9af0aa154df5078662ebc
Darin London <darin.london@duke.edu>
2013-11-18 10:37:27 -0500

    needed a secret key for new rails for some reason, ignoring the schema.rb generated by db:migrate

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Rads
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.secret_key_base = 'f3cf0735c8d340882c29727b61006b575e00b3c061040ce8abe270716ebf3474b7518c72faac743bc2ec17bca1a3768065a619221f19ad1024ddd2b1c268af4b'
   end
 end
