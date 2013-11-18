fb92387f1f3ba9e2353114e1416239a3ad4e7aff
Darrin Mann <darrin.mann@duke.edu>
2013-11-18 18:57:38 -0500

    Removing auto generated controller tests for cores_controller_test

--- test/controllers/cores_controller_test.rb
+++ test/controllers/cores_controller_test.rb
@@ -5,45 +5,13 @@ class CoresControllerTest < ActionController::TestCase
     @core = cores(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:cores)
-  end
+  context 'Not Authenticated User' do
+  end #Not Authenticated User
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  #switch to user
+  context 'Non-RepositoryUser' do
+  end #Non-RepositoryUser
 
-  test "should create core" do
-    assert_difference('Core.count') do
-      post :create, core: { creator_id: @core.creator_id, description: @core.description, name: @core.name }
-    end
-
-    assert_redirected_to core_path(assigns(:core))
-  end
-
-  test "should show core" do
-    get :show, id: @core
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @core
-    assert_response :success
-  end
-
-  test "should update core" do
-    patch :update, id: @core, core: { creator_id: @core.creator_id, description: @core.description, name: @core.name }
-    assert_redirected_to core_path(assigns(:core))
-  end
-
-  test "should destroy core" do
-    assert_difference('Core.count', -1) do
-      delete :destroy, id: @core
-    end
-
-    assert_redirected_to cores_path
-  end
+  context 'RepositoryUser' do
+  end #RepositoryUser
 end
