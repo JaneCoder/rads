a322040738d3f1ea52a8c3fcf5f5c7143e80ea59
Darrin Mann <darrin.mann@duke.edu>
2013-11-20 15:42:06 -0500

    Make with the go.

--- app/models/core.rb
+++ app/models/core.rb
@@ -1,11 +1,7 @@
 class Core < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
-<<<<<<< HEAD
   has_many :core_memberships, inverse_of: :core
-=======
-  has_many :core_memberships
   has_one :core_user
->>>>>>> 6e649ae9a75d46827c9fb60c1267ca3447306144
 
   validates_presence_of :name
   validates_presence_of :creator_id
